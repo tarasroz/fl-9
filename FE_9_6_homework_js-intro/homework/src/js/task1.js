b4ce61b1a3be638153253d93b3b5f1a144a5ec89
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-19 03:08:43 +0300

    add some round types

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,6 +1,6 @@
 // Your code goes here
-let priceUnDiscount = prompt('Input amount of money: ', '4950');
-let discountNumber = prompt('Input discount, %: ', '30');
+let priceUnDiscount = +prompt('Input amount of money: ', '4950');
+let discountNumber = +prompt('Input discount, %: ', '30');
 
 if (priceUnDiscount < 0) {
     console.log('’Invalid data’');
@@ -8,6 +8,22 @@ if (priceUnDiscount < 0) {
 
     let priceDiscount = priceUnDiscount - priceUnDiscount * discountNumber / 100;
     let saveNumber = priceUnDiscount - priceDiscount;
+
+    let getDecimal = priceDiscount - Math.floor(priceDiscount);
+    let expoNumber = 9e-3;
+
+    if (getDecimal === 0) {
+        priceDiscount = priceDiscount.toFixed();
+        saveNumber = Math.round(saveNumber);
+
+    } else if (priceDiscount % 1 <= expoNumber) {
+        priceDiscount = ~~priceDiscount;
+        saveNumber = saveNumber ^ 0;
+
+    } else {
+        priceDiscount = Math.floor(priceDiscount * 100) / 100;
+        saveNumber = Math.floor(saveNumber * 100) / 100;
+    }
 
     console.log('Price without discount: ' + priceUnDiscount + '\n' +
         'Discount: ' + discountNumber + '%' + '\n' +
