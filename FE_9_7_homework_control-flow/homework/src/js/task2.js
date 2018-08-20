57fb186e68259aa5b0b90e762ae948c62067e3c3
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-21 01:56:40 +0300

    a milioner

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,1 +1,47 @@
 // Your code goes here
+
+let start = confirm('Do you want to play a game?');
+
+if (!start) {
+    alert('You did not become a millionaire, but can.');
+} else {
+    let num_1 = 0;
+    let num_2 = 5;
+    let numHelp = 1;
+
+    let min = Math.ceil(num_1);
+    let max = Math.floor(num_2);
+
+    //could use object !!
+    let attemptI = 10;
+    let attemptII = 5;
+    let attemptIII = 2;
+
+    let attempt = [attemptI, attemptII, attemptIII];
+
+    let sumAttempt = 0;
+
+    for (let i = 0; i < attempt.length; i++) {
+        let randomNum = Math.floor(Math.random() * (max - min + numHelp)) + min;
+
+        let gessNum = +prompt('Enter a number from 0 to 5');
+
+        if (randomNum === gessNum) {
+
+            sumAttempt += +attempt[i];
+            // continue;
+        }
+    }
+    alert(`Thank you for a game. Your prize is: ${sumAttempt}$`);
+
+    let goOn = confirm('Do U want to play again?');
+
+    if (goOn) {
+        alert(`Congratulation!   Your prize is: ${sumAttempt}$ Do you want to continue?`);
+    } else {
+        alert(`Thank you for a game. Your prize is: ${sumAttempt}$`);
+        goOn = confirm('Do U want to play again?');
+
+        // if (goOn) {}
+    }
+}
