7e1d99a8e99b987066c9197d218f6fbd6292f0cc
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-23 15:08:06 +0300

    getMin();

--- FE_9_8_homework_functions/homework/src/getMin.js
+++ FE_9_8_homework_functions/homework/src/getMin.js
@@ -1,29 +1,17 @@
 // Task #3
 // Write a function - getMin
-// It should accept arbitrary number of integer arguments and returns the one with the smallest value.
-// For example: getMin(3, 0, -3); // => -3
-// Tip: since arguments is like array, you can use simple iteration over it
-// and use arguments[ i ] to get the argument of a given index
+function getMin(arg) {
 
-// function isBigger(firstArg, secondArg) {
-
-//     return firstArg > secondArg;
-// }
-
-// isBigger();
-
-// let arguments = [3, 0, -3];
-
-function getMin([arguments]) {
-    arguments = [];
     let minArg = 0;
-    for (let i = Infinity; i < arguments.lenght; i++) {
-        if (arguments[0] < arguments[i]) {
-            minArg = arguments[0];
-        } else {
-            minArg = arguments[i];
+    // Tip: since arguments is like array, you can use simple iteration over it
+    for (let i = 0; i < arg.length; i++) {
+        // and use arguments[ i ] to get the argument of a given index
+        if (minArg > arg[i]) {
+            minArg = arg[i];
         }
     }
+    //It should accept arbitrary number of integer arguments and returns the one with the smallest value.
+    return parseInt(minArg);
 }
-
-getMin([3, 0, -3]);
+// For example: getMin(3, 0, -3); // => -3
+getMin([]);
