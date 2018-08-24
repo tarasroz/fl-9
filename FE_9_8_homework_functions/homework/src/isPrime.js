29478b8bdc0cb7cd1a811e8529c1029e750f2fc4
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-24 04:08:23 +0300

    with commits

--- FE_9_8_homework_functions/homework/src/isPrime.js
+++ FE_9_8_homework_functions/homework/src/isPrime.js
@@ -1,1 +1,19 @@
-// Your code goes here
+// Task #4
+// Write a function - isPrime
+function isPrime(primeNum) {
+
+    let simple = true;
+
+    for (let i = 2; i < primeNum; i++) {
+
+        if (primeNum % i === 0) {
+
+            simple = false;
+        }
+
+    }
+    // It should accept one integer argument and returns true if it is prime number or false otherwise
+    return simple;
+}
+// For example: isPrime(5); // => true
+isPrime();
