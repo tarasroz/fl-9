f289b0cfa786d077dd05bc044b51315d34f42dee
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-26 03:42:27 +0300

    let arrArg = [];

--- FE_9_8_homework_functions/homework/src/getClosestToZero.js
+++ FE_9_8_homework_functions/homework/src/getClosestToZero.js
@@ -1,1 +1,20 @@
-// Your code goes here
+/* Task #5 */
+
+let arrArg = [];
+//Write a function - getClosestToZero
+let getClosetToZero = () => {
+
+        let min = arrArg[1];
+        //It should accept arbitrary number of integer arguments 
+        for (let i = 0; i < arrArg.length; i++) {
+            //Tip: Math.abs() might be helpful
+            if (Math.abs(arrArg[i]) < min) {
+                min = arrArg[i];
+            }
+        }
+        //and returns one closest to 0 (zero).
+        // console.log(min);
+        return min;
+    }
+    //For example: getClosestToZero(9, 5, -4, -9); // => -4
+getClosetToZero();
