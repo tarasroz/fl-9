92a0d77e770504ced73953bef68484930fe67bba
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-26 23:24:05 +0300

    a lot of const

--- FE_9_8_homework_functions/homework/src/reverseNumber.js
+++ FE_9_8_homework_functions/homework/src/reverseNumber.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const MATRIX = { ROW_1: 123, ROW_2: 456, ROW_3: 789 };
+const ROW_NUM = 198;
+const COL_NUM = 594;
+
+function reverseNumber(reverseArr) {
+
+    reverseArr < 0 ? reverseArr = -reverseArr : reverseArr;
+
+    if (reverseArr === MATRIX.ROW_1 || reverseArr === MATRIX.ROW_2 || reverseArr === MATRIX.ROW_3) {
+        reverseArr += ROW_NUM;
+
+    } else {
+        reverseArr += COL_NUM;
+
+    }
+    console.log(reverseArr);
+}
+
+reverseNumber();
