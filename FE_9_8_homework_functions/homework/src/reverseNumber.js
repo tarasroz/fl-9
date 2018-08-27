a370b00d0e95f123ea9c7a3aebe357c9ebf5e98c
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-27 15:36:04 +0300

    differ Number

--- FE_9_8_homework_functions/homework/src/reverseNumber.js
+++ FE_9_8_homework_functions/homework/src/reverseNumber.js
@@ -1,19 +1,28 @@
-const MATRIX = { ROW_1: 123, ROW_2: 456, ROW_3: 789 };
-const ROW_NUM = 198;
-const COL_NUM = 594;
+// Task #6
 
+// Write a function - reverseNumber
 function reverseNumber(reverseArr) {
+    let reverseSum = '',
+        reverseAbs = 0,
+        reverseDec = 0;
+    const Find_Decimal = 10,
+        For_Big = 4;
 
-    reverseArr < 0 ? reverseArr = -reverseArr : reverseArr;
+    reverseAbs = Math.abs(reverseArr);
 
-    if (reverseArr === MATRIX.ROW_1 || reverseArr === MATRIX.ROW_2 || reverseArr === MATRIX.ROW_3) {
-        reverseArr += ROW_NUM;
+    for (let i = 0; i < reverseAbs * For_Big; i++) {
 
-    } else {
-        reverseArr += COL_NUM;
-
+        reverseAbs = reverseAbs / Find_Decimal;
+        reverseDec = Math.trunc((reverseAbs - Math.floor(reverseAbs)) * Find_Decimal);
+        reverseSum += reverseDec;
     }
-    console.log(reverseArr);
+    //Numbers should preserve their sign: i.e. a negative number should still be negative when reversed.
+    reverseArr < 0 ? reverseSum = -reverseSum : reverseSum;
+    // It should accept an integer and return it`s reversed version.
+    return parseInt(reverseSum);
 }
-
+// For example: 
+// reverseNumber(123); // => 321
+// reverseNumber(-456); // => -654
+// reverseNumber(1000); // => 1
 reverseNumber();
