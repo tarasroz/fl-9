b47c8a5b1a776d6861489819d7f9c86e4fd3f07d
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-08-27 15:52:24 +0300

    use isBigger

--- FE_9_8_homework_functions/homework/src/isSmaller.js
+++ FE_9_8_homework_functions/homework/src/isSmaller.js
@@ -1,8 +1,13 @@
 // Task #2
+function isBigger(firstArg, secondArg) {
+
+    return firstArg > secondArg;
+}
+
 
 function isSmaller(firstArg, secondArg) {
 
-    return firstArg < secondArg;
+    return !isBigger(firstArg, secondArg);
 }
 
 isSmaller();
