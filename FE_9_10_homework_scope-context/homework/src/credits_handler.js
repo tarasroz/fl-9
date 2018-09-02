5d61d9043f5df37bfb600cc8aae2d0f06b6ef557
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-09-02 21:52:36 +0300

    change in transferCredits

--- FE_9_10_homework_scope-context/homework/src/credits_handler.js
+++ FE_9_10_homework_scope-context/homework/src/credits_handler.js
@@ -6,10 +6,13 @@ function userCard(key) {
     let transactionLimit = 100;
     let historyLog = [];
     let taxPercent = 0.005;
-    let event = new Date(Date.UTC( /*2018, 06, 27, 3, 24, 53*/ ));
+    let event = new Date(Date.UTC( /* 2018, 06, 27, 3, 24, 53 */ ));
+
 
     function getCardOptions() {
+
         return {
+
             key,
             balance,
             transactionLimit,
@@ -17,7 +20,9 @@ function userCard(key) {
         }
     }
 
+
     function putCredits(creditsPut) {
+
         balance += creditsPut;
         historyLog[historyLog.length] = {
             operationType: 'Received credits',
@@ -25,23 +30,31 @@ function userCard(key) {
             operationTime: event.toLocaleString('en-GB', { timeZone: 'UTC' })
         }
     }
-    putCredits( /*150*/ );
+
+    putCredits( /* 150 */ );
+
 
     function takeCredits(creditsTake) {
+
         if (transactionLimit >= creditsTake && balance >= creditsTake) {
+
             balance -= creditsTake;
             historyLog[historyLog.length] = {
                 operationType: 'Withdrawal of credit',
                 credits: creditsTake,
                 operationTime: event.toLocaleString('en-GB', { timeZone: 'UTC' })
             }
+
         } else {
             console.log('You could not take credit greater than transaction limit and remaining balance!')
         }
     }
-    takeCredits( /*100*/ );
+
+    takeCredits( /* 100 */ );
+
 
     function setTransactionLimit(creditsLimit) {
+
         transactionLimit = creditsLimit;
         historyLog[historyLog.length] = {
             operationType: 'Transaction limit change',
@@ -49,14 +62,19 @@ function userCard(key) {
             operationTime: event.toLocaleString('en-GB', { timeZone: 'UTC' })
         }
     }
-    setTransactionLimit( /*5000*/ );
 
-    function transferCredits(creditsTransfer /*= 50*/ , cardRecipient = card1) {
+    setTransactionLimit( /* 5000 */ );
+
+
+    function transferCredits(creditsTransfer /* = 50 */ , cardRecipient = card1) {
+
         if (transactionLimit >= creditsTransfer && balance >= creditsTransfer) {
+
             let sumTransfer = creditsTransfer + creditsTransfer * taxPercent;
-            let userTransfer = userCard(cardRecipient);
+
             balance -= sumTransfer;
-            userTransfer.putCredits(creditsTransfer);
+            cardRecipient.putCredits(creditsTransfer);
+
         } else {
             console.log('You could not take credit greater than transaction limit and remaining balance!')
         }
@@ -64,6 +82,7 @@ function userCard(key) {
 
 
     return {
+
         key,
         balance,
         transactionLimit,
@@ -86,13 +105,16 @@ class UserAccount {
         this.maxUserCard = 3
     }
 
+
     addCard() {
         this.cardsArray.length < this.maxUserCard ?
             this.cardsArray[this.cardsArray.length] = userCard(++this.cardsArray.length) :
             console.log('NOTE. User should have no more 3 cards!')
     }
 
+
     getCardByKey(key) {
+
         return this.cardsArray[--key]
     }
 }
@@ -109,14 +131,14 @@ user.addCard()
 console.log(user.cardsArray);
 
 let card1 = user.getCardByKey(1);
-let card2 = user.getCardByKey( /*2*/ );
+let card2 = user.getCardByKey( /* 2 */ );
 
 
-card1.putCredits( /*500*/ );
-card1.setTransactionLimit( /*800*/ );
-card1.transferCredits( /*300, */ card2);
+card1.putCredits( /* 500 */ );
+card1.setTransactionLimit( /* 800 */ );
+card1.transferCredits( /* 300, */ card2);
 
-card2.takeCredits( /*50*/ );
+card2.takeCredits( /* 50 */ );
 
 console.log(card1.getCardOptions());
 console.log(card2.getCardOptions());
