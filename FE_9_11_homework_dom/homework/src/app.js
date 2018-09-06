8908880dae202bfeb77357c6ce7b78627f9c89de
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-09-06 17:04:01 +0300

    code is a little less

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,27 +1,26 @@
 const rootNode = document.getElementById('#root');
-
 let addNewItem = document.querySelector('.addNewItem'),
-    addButton = document.querySelector('.addButton'),
-    listDo = document.querySelector('.listDiv'),
+    addIcon = document.querySelector('#addIcon'),
+    listDo = document.querySelector('.listDo'),
     maxNum = 11,
     itemPlus = 1;
 
 
 addNewItem.addEventListener('change', function() {
     if (addNewItem.value === '') {
-        addButton.disabled = true;
+        addIcon.disabled = true;
     } else if (addNewItem.value !== '') {
-        addButton.disabled = false;
+        addIcon.disabled = false;
     }
 
-    addButton.onclick = function() {
+    addIcon.onclick = function() {
         let itemLabel = addNewItem.value;
         addNewItem.value = '';
-        addButton.disabled = true;
+        addIcon.disabled = true;
         ++itemPlus;
 
         if (itemPlus <= maxNum) {
-            addButton.disabled = true;
+            addIcon.disabled = true;
             document.querySelector('.headName').textContent = 'TODO Cat List';
 
             let listLabel = document.createElement('li');
@@ -31,38 +30,30 @@ addNewItem.addEventListener('change', function() {
             let textLabel = document.createElement('span');
             textLabel.appendChild(document.createTextNode(itemLabel));
 
-            let checkBlock = document.createElement('button');
-            checkBlock.setAttribute('class', 'checkBox');
-
             let checkBlockIcon = document.createElement('i');
             checkBlockIcon.setAttribute('class', 'material-icons');
             checkBlockIcon.appendChild(document.createTextNode('check_box_outline_blank'));
-            checkBlock.appendChild(checkBlockIcon);
-
-            let deleteBlock = document.createElement('button');
-            deleteBlock.setAttribute('class', 'delButton');
 
             let deleteBlockIcon = document.createElement('i');
             deleteBlockIcon.setAttribute('class', 'material-icons');
             deleteBlockIcon.appendChild(document.createTextNode('delete'));
-            deleteBlock.appendChild(deleteBlockIcon);
 
-            listLabel.appendChild(checkBlock);
+            listLabel.appendChild(checkBlockIcon);
             listLabel.appendChild(textLabel);
-            listLabel.appendChild(deleteBlock);
+            listLabel.appendChild(deleteBlockIcon);
 
             listDo.appendChild(listLabel);
 
-            checkBlock.onclick = function() {
+            checkBlockIcon.onclick = function() {
                 checkBlockIcon.textContent = 'check_box';
             };
 
-            deleteBlock.onclick = function() {
+            deleteBlockIcon.onclick = function() {
                 listLabel.remove();
                 --itemLabel;
             };
         } else {
-            addButton.disabled = true;
+            addIcon.disabled = true;
             document.querySelector('.headName').textContent = 'Maximum item per list are created';
         }
     }
@@ -84,8 +75,6 @@ listDo.addEventListener('dragover', function(event) {
 listDo.addEventListener('drop', function(event) {
     if (event.target.className === 'li') {
         event.preventDefault();
-        listDo.insertBefore(dragDrop, event.target);
+        listDo.insertBefore(dragDrop, event.target.nextSibling);
     }
 });
-
-// rootNode.appendChild( /* Append your list item node*/ );
