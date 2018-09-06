4eecc0b9c2427262a4ad4e1b3461a8d0a02cb992
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-09-06 13:09:21 +0300

    one main function

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,108 +1,91 @@
-const rootNode = document.getElementById('root');
+const rootNode = document.getElementById('#root');
 
-let addItemNew = document.querySelector('.addNewItem'),
+let addNewItem = document.querySelector('.addNewItem'),
     addButton = document.querySelector('.addButton'),
-    list = document.querySelector('.list'),
-    addDiv = document.getElementsByClassName('.addItem'),
+    listDo = document.querySelector('.listDiv'),
     maxNum = 10,
     itemPlus = 1;
 
-// main
 
-addItemNew.addEventListener('change', function() {
-
-    if (addItemNew.value !== '') {
-        addButton.disabled = false;
-    } else {
-
+addNewItem.addEventListener('change', function() {
+    if (addNewItem.value === '') {
         addButton.disabled = true;
+    } else if (addNewItem.value !== '') {
+        addButton.disabled = false;
+    }
+
+    addButton.onclick = function() {
+        let itemLabel = addNewItem.value;
+        addNewItem.value = '';
+        addButton.disabled = true;
+        itemPlus++;
+
+        if (itemPlus <= maxNum) {
+            addButton.disabled = true;
+            document.querySelector('.headName').textContent = 'TODO Cat List';
+
+            let listLabel = document.createElement('li');
+            listLabel.setAttribute('class', 'li');
+            listLabel.setAttribute('draggable', true);
+
+            let textLabel = document.createElement('span');
+            textLabel.appendChild(document.createTextNode(itemLabel));
+
+            let checkBlock = document.createElement('button');
+            checkBlock.setAttribute('class', 'checkBox');
+
+            let checkBlockIcon = document.createElement('i');
+            checkBlockIcon.setAttribute('class', 'material-icons');
+            checkBlockIcon.appendChild(document.createTextNode('check_box_outline_blank'));
+            checkBlock.appendChild(checkBlockIcon);
+
+            let deleteBlock = document.createElement('button');
+            deleteBlock.setAttribute('class', 'delButton');
+
+            let deleteBlockIcon = document.createElement('i');
+            deleteBlockIcon.setAttribute('class', 'material-icons');
+            deleteBlockIcon.appendChild(document.createTextNode('delete'));
+            deleteBlock.appendChild(deleteBlockIcon);
+
+            listLabel.appendChild(checkBlock);
+            listLabel.appendChild(textLabel);
+            listLabel.appendChild(deleteBlock);
+
+            listDo.appendChild(listLabel);
+
+            checkBlock.onclick = function() {
+                checkBlockIcon.textContent = 'check_box';
+            };
+
+            deleteBlock.onclick = function() {
+                listLabel.remove();
+                --itemLabel;
+            };
+        } else {
+            addButton.disabled = true;
+            document.querySelector('.headName').textContent = 'Maximum item per list are created';
+        }
     }
 })
 
-addButton.onclick = function() {
-    let itemLabel = addItemNew.value;
-    addItem(itemLabel);
-    addItemNew.value = '';
-    addButton.disabled = true;
-    ++itemPlus;
-}
-
-function addItem(itemLabel) {
-
-    if (itemPlus <= maxNum) {
-        addButton.disabled = false;
-        document.querySelector('.headName').textContent = 'TODO Cat List';
-
-
-        let listLabel = document.createElement('li');
-        listLabel.setAttribute('class', 'li');
-        listLabel.setAttribute('draggable', true);
-
-
-        let textLabel = document.createElement('span');
-        textLabel.appendChild(document.createTextNode(itemLabel));
-
-
-        let checkBlock = document.createElement('button');
-        checkBlock.setAttribute('class', 'checkBox');
-
-        let checkBlockIcon = document.createElement('i');
-        checkBlockIcon.setAttribute('class', 'material-icons');
-        checkBlockIcon.appendChild(document.createTextNode('check_box_outline_blank'));
-        checkBlock.appendChild(checkBlockIcon);
-
-
-        let deleteBlock = document.createElement('button');
-        deleteBlock.setAttribute('class', 'delButton');
-
-        let deleteBlockIcon = document.createElement('i');
-        deleteBlockIcon.setAttribute('class', 'material-icons');
-        deleteBlockIcon.appendChild(document.createTextNode('delete'));
-        deleteBlock.appendChild(deleteBlockIcon);
-
-
-        listLabel.appendChild(checkBlock);
-        listLabel.appendChild(textLabel);
-        listLabel.appendChild(deleteBlock);
-
-
-        list.appendChild(listLabel);
-
-
-        checkBlock.onclick = function() {
-            checkBlockIcon.textContent = 'check_box';
-        };
-        deleteBlock.onclick = () => {
-            listLabel.remove();
-            --itemLabel;
-        };
-    } else {
-
-        addButton.disabled = true;
-        document.querySelector('.headName').textContent = 'Maximum item per list are created';
-    }
-}
-
-// Drag & drop action
-
 let dragDrop = null;
 
-list.addEventListener('dragstart', function(e) {
-    dragDrop = e.target;
+listDo.addEventListener('dragstart', function(event) {
+    dragDrop = event.target;
 });
 
-list.addEventListener('dragover', function(e) {
-
-    if (e.target.className === 'li') {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = 'move';
+listDo.addEventListener('dragover', function(event) {
+    if (event.target.className === 'li') {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
     }
 });
 
-list.addEventListener('drop', function(e) {
-
-    if (e.target.className === 'li') {
-        e.preventDefault();
-        list.insertBefore(dragDrop, e.target);
+listDo.addEventListener('drop', function(event) {
+    if (event.target.className === 'li') {
+        event.preventDefault();
+        listDo.insertBefore(dragDrop, event.target);
     }
 });
+
+// rootNode.appendChild( /* Append your list item node*/ );
