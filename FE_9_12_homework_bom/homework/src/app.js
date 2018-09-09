49fdba818e66495c7eb6754b3cde9370ff200831
Taras Kozlovskyi <tarasroz@yahoo.com>
2018-09-10 02:17:52 +0300

    better changes

--- FE_9_12_homework_bom/homework/src/app.js
+++ FE_9_12_homework_bom/homework/src/app.js
@@ -4,13 +4,13 @@ const todoItems = [
     { isDone: false, id: 12345, description: 'Todo 1' }
 ];
 
-let toModify = null,
-    addBtn = document.getElementById('addBtn'),
+let addBtn = document.getElementById('addBtn'),
     cancel = document.getElementById('cancel'),
     save = document.getElementById('save'),
     headerH1 = document.getElementsByTagName('h1'),
     text = document.querySelector('input'),
-    ulList = document.querySelector('ul');
+    ulList = document.querySelector('ul'),
+    toModify = null;
 
 const NUM_0 = 0,
     NUM_1 = 1,
@@ -22,66 +22,76 @@ save.addEventListener('click', saveChanges);
 window.addEventListener('hashchange', hashChange);
 
 
-
-function addPage() {
-    window.location.hash = '/add';
-}
-
 function mainPage() {
     window.location.hash = '';
     history.pushState('', document.title, window.location.pathname);
 }
 
-function saveChanges() {
+function addPage() {
+    window.location.hash = '/add';
+}
+
+function saveChanges(addChildren) {
+
     if (window.location.hash === '#/add') {
-        generate(text.value);
-    } else {
-        let addChildren = toModify.childNodes;
-        addChildren[NUM_2].innerHTML = text.value;
+        doIt(text.value);
+
+    } else if (window.location.hash !== '#/add') {
+        addChildren = toModify.childNodes;
+        addChildren[NUM_2].innerText = text.value;
     }
+
     text.value = '';
     window.location.hash = '';
     history.pushState('', document.title, window.location.pathname);
 }
 
-function modBlock() {
-    let turn;
-    if (event.target.tagName === 'SPAN') {
-        turn = event.target.parentNode;
-    } else {
-        return;
+function modBlock(e, turn, modifyChildren) {
+
+    if (e.target.tagName === 'SPAN') {
+        turn = e.target.parentNode;
+
+    } else if (e.target.tagName !== 'SPAN') {
+
+        return this
     }
+
     window.location.hash = '/modify:';
-    let modifyChildren = turn.childNodes;
-    text.value = modifyChildren[NUM_2].innerHTML;
+    modifyChildren = turn.childNodes;
+    text.value = modifyChildren[NUM_2].innerText;
     toModify = turn;
 }
 
-function delBlock() {
-    let turnOff = event.target.parentNode;
-    turnOff.remove();
-}
-
-function checkFun(event) {
-    event.target.setAttribute('src', 'assets/img/done-s.png');
-    let checkSpan = event.target.parentNode.nextSibling;
+let checkFun = (e, checkSpan) => {
+    e.target.setAttribute('src', 'assets/img/done-s.png');
+    checkSpan = e.target.parentNode.nextSibling;
     checkSpan.style.backgroundColor = '#7f7f7f';
 }
 
-function hashChange() {
-    let hashChang = window.location.hash;
-    let hashItem = document.querySelector('.task');
+let delBlock = (e, turnOff) => {
+    turnOff = e.target.parentNode;
+    turnOff.remove();
+}
+
+function hashChange(hashChang, hashItem) {
+    hashChang = window.location.hash;
+    hashItem = document.querySelector('.task');
+
     if (hashChang === '#/add' || hashChang === '#/modify:') {
         headerH1[NUM_0].style = 'display: none';
+
         if (hashChang === '#/add') {
-            headerH1[NUM_2].style = 'display: none';
             headerH1[NUM_1].style = 'display: block';
+            headerH1[NUM_2].style = 'display: none';
+
         } else {
             headerH1[NUM_1].style = 'display: none';
             headerH1[NUM_2].style = 'display: block';
         }
+
         addBtn.style = 'display: none';
         hashItem.style = 'display: none';
+
         text.style = 'display: block';
         cancel.style = 'display: block';
         save.style = 'display: block';
@@ -90,49 +100,68 @@ function hashChange() {
         headerH1[NUM_0].style = 'display: block';
         headerH1[NUM_1].style = 'display: none';
         headerH1[NUM_2].style = 'display: none';
+
         addBtn.style = 'display: block';
         hashItem.style = 'display: block';
+
         text.style = 'display: none';
         cancel.style = 'display: none';
         save.style = 'display: none';
     }
 }
 
-function generate(value) {
-    function addAttribute(tag, attributesObject, markup) {
-        let newElement = document.createElement(tag);
-        if (Object.keys(attributesObject).length) {
-            for (let key in attributesObject) {
-                if (attributesObject.hasOwnProperty(key)) {
-                    newElement.setAttribute(key, attributesObject[key]);
-                }
+function addAttribute(tag, attributeObj, inside, newEl) {
+    newEl = document.createElement(tag);
+
+    if (Object.keys(attributeObj).length) {
+
+        for (let key in attributeObj) {
+
+            if (attributeObj.hasOwnProperty(key)) {
+                newEl.setAttribute(key, attributeObj[key]);
             }
         }
-        if (markup && typeof markup === 'string') {
-            newElement.innerHTML = markup
-        }
-        return newElement;
     }
 
-    if (value === '') {
-        return;
+    if (inside && typeof inside === 'string') {
+        newEl.innerText = inside
     }
-    let createLi = document.createElement('li');
 
-    let emptybox = addAttribute('input', { type: 'checkbox', id: 'thing', style: 'display: none' });
+    return newEl
+}
+
+function doIt(value, createLi, emptybox, labAttr, check, spanText, delImg) {
+    addAttribute;
+
+    createLi = document.createElement('li');
+
+    emptybox = addAttribute('input', {
+        type: 'checkbox',
+        id: 'emptyLi',
+        style: 'display: none'
+    });
     createLi.appendChild(emptybox);
 
-    let labAttr = addAttribute('label', { for: 'thing' });
+    labAttr = addAttribute('label', {
+        for: 'emptyLi'
+    });
 
-    let check = addAttribute('img', { src: 'assets/img/todo-s.png', alt: 'logo' })
+    check = addAttribute('img', {
+        src: 'assets/img/todo-s.png',
+        alt: 'logo-todo'
+    })
     labAttr.appendChild(check);
 
     createLi.appendChild(labAttr);
 
-    let spanText = addAttribute('span', {}, value);
+    spanText = addAttribute('span', {}, value);
     createLi.appendChild(spanText);
 
-    let delImg = addAttribute('img', { src: 'assets/img/remove-s.jpg', class: 'deleteImg', alt: 'logo' });
+    delImg = addAttribute('img', {
+        src: 'assets/img/remove-s.jpg',
+        class: 'deleteImg',
+        alt: 'logoRemove'
+    });
     createLi.appendChild(delImg);
 
     ulList.appendChild(createLi);
@@ -140,4 +169,9 @@ function generate(value) {
     createLi.addEventListener('click', modBlock);
     delImg.addEventListener('click', delBlock);
     check.addEventListener('click', checkFun);
+
+    if (value === '') {
+
+        return this
+    }
 }
